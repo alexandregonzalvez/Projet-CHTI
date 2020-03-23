d9439fc3f6e8635551e6becf98d3a91b9c2f6cf0
Mathieu Dechambe <dechambe.mathieu@orange.fr>
2020-03-23 22:48:25 +0100

    added asm

---
+++ assembleur/PROJ_UN/Src/Carre.s
@@ -0,0 +1,17 @@
+	thumb
+	area	moncode, code, readonly
+	export timer_callback
+
+timer_callback proc
+GPIOB_BSRR	equ	0x40010C10	; Bit Set/Reset register
+; mise a 1 de PB1
+	ldr	r3, =GPIOB_BSRR
+	mov	r1, #0x00000002
+	str	r1, [r3]
+; mise a zero de PB1
+	ldr	r3, =GPIOB_BSRR
+	mov	r1, #0x00020000
+	str	r1, [r3]
+; N.B. le registre BSRR est write-only, on ne peut pas le relire
+	endp
+	end
