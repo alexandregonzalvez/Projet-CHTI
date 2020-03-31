32cda39cb6c1d0d5231774aa64263f5fad65e5f7
Mathieu Dechambe <dechambe.mathieu@orange.fr>
2020-03-31 16:04:10 +0200

    changes to principal.c

--- assembleur/DFT/Src/principal.c
+++ assembleur/DFT/Src/principal.c
@@ -1,7 +1,5 @@
 #include "gassp72.h"
 
-float somme(int index);
-
 int m2k(int k, u16 * tabSig);
 
 extern u16 TabSig;
@@ -9,8 +7,7 @@ extern u16 TabSig;
 int main(void)
 {     
     m2k(1, &TabSig);
-    
-    
+        
     while    (1)
         {
         }
